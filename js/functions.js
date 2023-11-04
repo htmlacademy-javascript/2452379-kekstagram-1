b2384a764c28d6ce984cae7d6457ba879630966a
kartafil <allan.abasov@gmail.com>
2023-11-04 13:37:48 +0300

    Remake

--- js/functions.js
+++ js/functions.js
@@ -1,76 +1,70 @@
-const stringFunctions = {
-  isPolyndrom(str) {
-    str = String(str).replaceAll(' ', '').toLowerCase();
+const isPolyndrom = (str) => {
+  str = String(str).replaceAll(' ', '').toLowerCase();
 
-    for (let i = 0; i < Math.floor(str.length / 2); i++) {
-      if (str.at(i) !== str.at(-(i + 1))) {
-        return false;
-      }
+  for (let i = 0; i < Math.floor(str.length / 2); i++) {
+    if (str.at(i) !== str.at(-(i + 1))) {
+      return false;
     }
-
-    return true;
-  },
-  getAllNumbers(str) {
-    let result = '';
-    str = String(str).replaceAll(' ', '');
-
-    for (let i = 0; i < str.length; i++) {
-      if (!isNaN(parseInt(str[i], 10))) {
-        result += str[i];
-      }
-    }
-
-    return parseInt(result, 10);
-  },
-  pushString(str, maxLength, pushStr) {
-    const availableLength = maxLength - str.length;
-
-    if (availableLength <= 0) {
-      return str;
-    }
-
-    const stringToAdd = pushStr.repeat(Math.floor(availableLength / pushStr.length)) + pushStr.substring(0, availableLength % pushStr.length);
-
-    //let stringToAdd = '';
-    //
-    //for (let i = 0; i < availableLength; i++) {
-    //  stringToAdd += pushStr[i % pushStr.length];
-    //}
-
-    return stringToAdd + str;
-  },
-  isLessOrEqual(str, maxLength) {
-    return str.length <= maxLength;
   }
+
+  return true;
 };
 
-let funcResult = stringFunctions.isPolyndrom(1331);
+const getAllNumbers = (str) => {
+  let result = '';
+  str = String(str).replaceAll(' ', '');
+
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(parseInt(str[i], 10))) {
+      result += str[i];
+    }
+  }
+
+  return parseInt(result, 10);
+};
+
+const pushString = (str, maxLength, pushStr) => {
+  const availableLength = maxLength - str.length;
+
+  if (availableLength <= 0) {
+    return str;
+  }
+
+  const stringToAdd = pushStr.repeat(Math.floor(availableLength / pushStr.length)) + pushStr.substring(0, availableLength % pushStr.length);
+
+  return stringToAdd + str;
+};
+
+const isLessOrEqual = (str, maxLength) => str.length <= maxLength;
+
+
+let funcResult = isPolyndrom(1331);
 window.console.log(funcResult);
-funcResult = stringFunctions.isPolyndrom('Кекс');
+funcResult = isPolyndrom('Кекс');
 window.console.log(funcResult);
-funcResult = stringFunctions.isPolyndrom('Лёша на полке клопа нашёл ');
+funcResult = isPolyndrom('Лёша на полке клопа нашёл ');
 window.console.log(funcResult);
 
 
-funcResult = stringFunctions.getAllNumbers('Лёша на 2полке клопа нашёл 1');
+funcResult = getAllNumbers('Лёша на 2полке клопа нашёл 1');
 window.console.log(funcResult);
-funcResult = stringFunctions.getAllNumbers('Лёша на полке клопа нашёл ');
+funcResult = getAllNumbers('Лёша на полке клопа нашёл ');
 window.console.log(funcResult);
-funcResult = stringFunctions.getAllNumbers(-25.33727);
-window.console.log(funcResult);
-
-
-funcResult = stringFunctions.pushString('1', 4, '0');
-window.console.log(funcResult);
-funcResult = stringFunctions.pushString('q', 4, 'werty');
-window.console.log(funcResult);
-funcResult = stringFunctions.pushString('qwerty', 4, '0');
+funcResult = getAllNumbers(-25.33727);
 window.console.log(funcResult);
 
 
-funcResult = stringFunctions.isLessOrEqual('проверяемая строка', 20);
+funcResult = pushString('1', 4, '0');
 window.console.log(funcResult);
-funcResult = stringFunctions.isLessOrEqual('проверяемая строка', 18);
+funcResult = pushString('q', 4, 'werty');
 window.console.log(funcResult);
-funcResult = stringFunctions.isLessOrEqual('проверяемая строка', 10);
+funcResult = pushString('qwerty', 4, '0');
+window.console.log(funcResult);
+
+
+funcResult = isLessOrEqual('проверяемая строка', 20);
+window.console.log(funcResult);
+funcResult = isLessOrEqual('проверяемая строка', 18);
+window.console.log(funcResult);
+funcResult = isLessOrEqual('проверяемая строка', 10);
 window.console.log(funcResult);
