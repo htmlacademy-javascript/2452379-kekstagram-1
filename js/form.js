eb520ea383704b6dd685701cbcbd3895cf33b1b0
kartafil <allan.abasov@gmail.com>
2023-11-21 12:54:08 +0300

    validation error messages added

--- js/form.js
+++ js/form.js
@@ -1,6 +1,9 @@
 import { onEscKeydownDo } from './utils.js';
 
 const MAX_DESCRIPTION_SIZE = 140;
+const MAX_HASHTAGS_COUNT = 5;
+const MIN_HASHTAG_LENGTH = 1;
+const MAX_HASHTAG_LENGTH = 19;
 
 const uploadPictureForm = document.querySelector('#upload-select-image');
 const pictureInput = uploadPictureForm.querySelector('#upload-file');
@@ -11,6 +14,7 @@ const pictureDescription = pictureFieldsContainer.querySelector('.text__descript
 const closeButton = uploadPictureForm.querySelector('#upload-cancel');
 
 const hashtagRegex = /^#[a-zа-яё0-9]{1,19}$/i;
+const hashtagRegExp = new RegExp(`^#[a-zа-яё0-9]{${MIN_HASHTAG_LENGTH},${MAX_HASHTAG_LENGTH}}$`, 'i');
 
 const pristineConfig = {
   classTo: 'img-upload__field-wrapper',
@@ -49,17 +53,27 @@ const validateHashtags = (value) => {
     return true;
   }
 
-  const hashtags = value.trim().split(' ').filter((item) => item);
+  const hashtags = value.trim().split(/\s+/);
 
-  if (hashtags.length > 5) {
+  if (hashtags.length > MAX_HASHTAGS_COUNT) {
+    pristine.addError(pictureHashtags, `Не более ${MAX_HASHTAGS_COUNT} хештегов`);
     return false;
   }
 
   for (let i = 0; i < hashtags.length; i++) {
-    if (hashtags[i]
-      && (!(hashtags[i].lastIndexOf('#') === 0
-      && hashtagRegex.test(hashtags[i])
-      && !hashtags.some((item, index) => item === hashtags[i] && index !== i)))) {
+    if (!hashtags[i]) {
+      continue;
+    }
+    if (hashtags[i].lastIndexOf('#') !== 0) {
+      pristine.addError(pictureHashtags, 'Хештеги начинаются с # и разеляются пробелами');
+      return false;
+    }
+    if (!hashtagRegExp.test(hashtags[i])) {
+      pristine.addError(pictureHashtags, `Длина хештега ${MIN_HASHTAG_LENGTH}-${MAX_HASHTAG_LENGTH}. Нельзя использовать спецсимволы/пунктуацию/эмодзи`);
+      return false;
+    }
+    if (hashtags.some((item, index) => item === hashtags[i] && index !== i)) {
+      pristine.addError(pictureHashtags, 'Хештеги не могут повторяться');
       return false;
     }
   }
@@ -67,9 +81,14 @@ const validateHashtags = (value) => {
   return true;
 };
 const validateDescription = (value) => value.length < MAX_DESCRIPTION_SIZE;
+const getHashtagsErrorText = () => {
+  const errors = pristine.getErrors(pictureHashtags);
+  return errors[errors.length - 1];
+};
+const getDescriptionErrorText = `Не более ${MAX_DESCRIPTION_SIZE} символов`;
 
-pristine.addValidator(pictureDescription, validateDescription, 'huynya');
-pristine.addValidator(pictureHashtags, validateHashtags, 'huynya dvajdi');
+pristine.addValidator(pictureHashtags, validateHashtags, getHashtagsErrorText);
+pristine.addValidator(pictureDescription, validateDescription, getDescriptionErrorText);
 
 pictureInput.addEventListener('input', onPictureInput);
 closeButton.addEventListener('click', onCloseUploadPictureFormClick);
